6a02a72134497361eb925419ba214a06a0b7dba6
DharamTech <dharminderkumar0475@gmail.com>
2020-09-29 02:37:13 -0700

    first commit

---
+++ routes/userRouter.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const User = require("../models/userModel");
+
+router.post("/register", async (req, res) => {
+	try {
+	const { email, password, passwordCheck, displayName } = req.body;
+
+	// validate
+
+	if (!email || !password || !passwordCheck)
+		return res.status(400).json({ msg: "Not all fields have been entered."});
+	if (password.length < 5)
+		return res
+	      .status(400)
+	      .json({ msg: "The password needs to be at least 5 charachers long." });
+	if (password !== passwordCheck)
+		return res
+	      .status(400)
+	      .json({ msg: "Enter the same password twice for verification." });
+
+        const existingUser = await User.find({ email: email })
+        if (existingUser)
+         return res
+	      .status(400)
+	      .json({ msg: "An account with this email already exists." });
+        if (!displayName) displayName = email;
+
+} catch (err) {
+	res.status(500).json(err);
+}
+});
+
+module.exports = router;
